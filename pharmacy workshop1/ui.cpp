4c86f9e783937f7c58df9391ed6cf9912b455ccc
OOI XIEN XIEN <b032310183@student.utem.edu.my>
2024-09-21 22:22:38 +0800

    update ui.cpp files

--- pharmacy workshop1/ui.cpp
+++ pharmacy workshop1/ui.cpp
@@ -13,7 +13,7 @@ using namespace std;
 
 void ui::welcome()
 {
-    SetConsoleColor(1, 14);
+    //input file welcome txt
     ifstream in("welcome.txt");
     string line;
 
@@ -22,31 +22,26 @@ void ui::welcome()
 
     while (getline(in, line))
     {
-        cout << line << endl;
+        cout << line<< endl;
     }
-    ConnectionFunction();
-    this_thread::sleep_for(chrono::milliseconds(4000));
 
-    cout << endl << endl << endl;
-    system("cls");
-    // Set global background to yellow and text to light yellow
-    system("color ec");
-
-    // Print the rest of the content with custom colors
+    cout << endl << endl << endl ;
+    system("color e0");
     cout << "\t\t|_________________________________________________________________________________________________________________________________________________________|\n";
     cout << "\t\t|                                                                                                                                                         |\n";
     cout << "\t\t|                                                         Pharmacy Management System                                                                      |\n";
     cout << "\t\t|_________________________________________________________________________________________________________________________________________________________|\n";
     cout << "\t\t|---------------------------------------------------------------------------------------------------------------------------------------------------------|\n";
-    cout << "\t\t|  XXXXXXXXX       XX       XX        XXXX         XXXXXXXXX          XXX       XXX                  XXXX       XXXXXXXXX        XXXXX    XX       XX;    |\n";
-    cout << "\t\t|  XX       XX     XX       XX       XX  XX        XX       XX       XX   XX   XX XX                XX  XX      XX       XX    XX     XX   XX     XX      |\n";
-    cout << "\t\t|  XX       XX     XX       XX      XX    XX       XX       XX      XX     XX XX   XX              XX    XX     XX       XX    XX           XX   XX       |\n";
-    cout << "\t\t|  XXXXXXXXX       XXXXXXXXXXX     XX      XX      XXXXXXXXX       XX       XX      XX            XX      XX    XXXXXXXXX      XX              XX         |\n";
-    cout << "\t\t|  XX              XX       XX    XXXXXXXXXXXX     XX             XX                 XX          XXXXXXXXXXXX   XX             XX              XX         |\n";
-    cout << "\t\t|  XX              XX       XX   XX         XX     XX XX         XX                   XX        XX         XX   XX XX          XX              XX         |\n";
-    cout << "\t\t|  XX              XX       XX  XX           XX    XX   XX      XX                     XX      XX           XX  XX   XX        XX     XX       XX         |\n";
-    cout << "\t\t|  XX              XX       XX XX             XX   XX     XX   XX                       XX    XX             XX XX     XX        XXXXX         XX         |\n";
-
+    SetConsoleColor(11, 7);
+    cout << "\t\t|  XXXXXXXXX       XXXXXXXXX        XXXX         XXXXXXXXX          XXX       XXX                  XXXX       XXXXXXXXX        XXXXX    XX       XX;      |\n";
+    cout << "\t\t|  XX       XX     XX       XX     XX  XX        XX       XX       XX   XX   XX XX                XX  XX      XX       XX    XX     XX   XX     XX        |\n";
+    cout << "\t\t|  XX       XX     XX       XX    XX    XX       XX       XX      XX     XX XX   XX              XX    XX     XX       XX    XX           XX   XX         |\n";
+    cout << "\t\t|  XXXXXXXXX       XXXXXXXXX     XX      XX      XXXXXXXXX       XX       XX      XX            XX      XX    XXXXXXXXX      XX              XX           |\n";
+    cout << "\t\t|  XX              XX           XXXXXXXXXXXX     XX             XX                 XX          XXXXXXXXXXXX   XX             XX              XX           |\n";
+    cout << "\t\t|  XX              XX XX       XX         XX     XX XX         XX                   XX        XX         XX   XX XX          XX              XX           |\n";
+    cout << "\t\t|  XX              XX   XX    XX           XX    XX   XX      XX                     XX      XX           XX  XX   XX        XX     XX       XX           |\n";
+    cout << "\t\t|  XX              XX     XX XX             XX   XX     XX   XX                       XX    XX             XX XX     XX        XXXXX         XX           |\n";
+    system("color e0");
     cout << "\t\t|_________________________________________________________________________________________________________________________________________________________|\n";
     cout << "\t\t|---------------------------------------------------------------------------------------------------------------------------------------------------------|\n";
     cout << "\t\t| Press 1 to LOGIN                                                                                                                                        |" << endl;
